aed11d189d2998fafef36e61f26c12d3168f63b2
Roudique <roudique@gmail.com>
2018-12-27 20:38:06 -0500

    Add CSV.swift and Files as dependencies

--- Package.swift
+++ Package.swift
@@ -13,16 +13,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yaslab/CSV.swift.git", from: "2.3.1"),
+        .package(url: "https://github.com/johnsundell/files.git", from: "2.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ScryLogFileService",
-            dependencies: []),
+            dependencies: ["CSV", "Files"],
+            path: "Sources"),
         .testTarget(
             name: "ScryLogFileServiceTests",
-            dependencies: ["ScryLogFileService"]),
+            dependencies: ["CSV", "Files"]),
     ]
 )
