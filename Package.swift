8f4d64fa0e448dcd0694a09216fa99c44b2cd0e9
Roudique <roudique@gmail.com>
2018-12-28 21:13:43 -0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -14,17 +14,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/yaslab/CSV.swift.git", from: "2.3.1"),
-        .package(url: "https://github.com/johnsundell/files.git", from: "2.2.1")
+        .package(url: "https://github.com/johnsundell/files.git", from: "2.2.1"),
+        .package(url: "https://github.com/Roudique/ScryLogHTMLParser.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ScryLogFileService",
-            dependencies: ["CSV", "Files"],
+            dependencies: ["CSV", "Files", "ScryLogHTMLParser"],
             path: "Sources"),
         .testTarget(
             name: "ScryLogFileServiceTests",
-            dependencies: ["CSV", "Files"]),
+            dependencies: ["CSV", "Files", "ScryLogHTMLParser"]),
     ]
 )
--- Sources/ScryLogFileService/ScryLogFileService.swift
+++ Sources/ScryLogFileService/ScryLogFileService.swift
@@ -1,3 +1,14 @@
-struct ScryLogFileService {
-    var text = "Hello, World!"
+import Files
+import Foundation
+import ScryLogHTMLParser
+
+class FileService {
+    private let startFolder: Folder
+    
+    // MARK: - Public
+    
+    init?(startDirectoryPath: String) {
+        guard let folder = try? Folder(path: startDirectoryPath) else { return nil }
+        self.startFolder = folder
+    }
 }
